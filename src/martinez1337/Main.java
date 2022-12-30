15252929c4f49470289a0ac5fda3fce51196520b
Martinez1337 <90857594+Martinez1337@users.noreply.github.com>
2022-12-30 12:01:26 +0300

    Adding program entry point

---
+++ src/martinez1337/Main.java
@@ -0,0 +1,11 @@
+package martinez1337;
+
+/**
+ * Точка входа в программу.
+ */
+public class Main {
+    public static void main(String[] args) {
+        var logicService = new MasterLogicService();
+        logicService.mainProcess();
+    }
+}
